663604725a11c6787d8324d5ec8c26db169b2ac8
yangtao <istaotao@aliyun.com>
2019-07-01 11:24:13 +0800

    Update edm

--- src/myreact/01/src/App.js
+++ src/myreact/01/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Hi, 我来了</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
