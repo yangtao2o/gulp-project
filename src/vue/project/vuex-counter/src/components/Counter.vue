9c6b3d3978f08696534f0b6d677011643e575a8b
yangtao <istaotao@aliyun.com>
2020-02-12 17:59:53 +0800

    feat: new vuex-counter init

---
+++ src/vue/project/vuex-counter/src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+// 创建一个 Counter 组件
+// const Counter = {
+//   template: `<div>{{ count }}</div>`,
+//   computed: {
+//     count () {
+//       return store.state.count
+//     }
+//   }
+// }
+export default {
+  name: 'Counter',
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+    }
+  },
+}
+</script>
+
+
